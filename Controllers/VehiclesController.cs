fceeb80b14909000257718ef4b903259f1f3e220
devkiratu <dev.kiratu@gmail.com>
2021-04-22 15:25:42 +0300

    add API for Creating a Vehicle

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,35 @@
+using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+  [Route("/api/vehicles")]
+  public class VehiclesController : Controller
+  {
+    private readonly IMapper mapper;
+    private readonly VegaDbContext context;
+    public VehiclesController(IMapper mapper, VegaDbContext context)
+    {
+      this.context = context;
+      this.mapper = mapper;
+
+    }
+
+    [HttpPost]
+    public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+    {
+      var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+      vehicle.LastUpdate = DateTime.Now;
+      context.Vehicles.Add(vehicle);
+      context.SaveChanges();
+
+
+      return Ok(mapper.Map<Vehicle, VehicleResource>(vehicle));
+    }
+
+  }
+}

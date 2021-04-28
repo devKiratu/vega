c4ba9a15fdca278e55df5ce1bf50f85318f21cee
devkiratu <dev.kiratu@gmail.com>
2021-04-28 14:27:37 +0300

    implement client-side filtering of Vehicles by make

--- ClientApp/src/components/DisplayVehicles.js
+++ ClientApp/src/components/DisplayVehicles.js
@@ -3,6 +3,20 @@ import { Link, NavLink } from "react-router-dom";
 
 function DisplayVehicles() {
 	const [vehicles, setVehicles] = useState([]);
+	const [makes, setMakes] = useState([]);
+	const [allVehicles, setAllVehicles] = useState([]);
+
+	function filterVehicles(e) {
+		const id = e.target.value;
+		const filter = [...allVehicles];
+		if (id !== "") {
+			setVehicles(filter.filter((v) => v.make.id == id));
+		} else {
+			setVehicles(allVehicles);
+		}
+
+		// console.log(id);
+	}
 
 	async function getVehicles() {
 		const res = await fetch("api/vehicles", {
@@ -10,11 +24,19 @@ function DisplayVehicles() {
 		});
 		const data = await res.json();
 		setVehicles(data);
-		console.log("the vehicles are ", data);
+		setAllVehicles(data);
+		// console.log("the vehicles are ", data);
+	}
+
+	async function GetMakes() {
+		const res = await fetch("api/makes");
+		const data = await res.json();
+		setMakes(data);
 	}
 
 	useEffect(() => {
 		getVehicles();
+		GetMakes();
 	}, []);
 
 	return (
@@ -22,6 +44,28 @@ function DisplayVehicles() {
 			<NavLink to="/vehicles/new">
 				<button className="btn btn-primary">Add Vehicle</button>
 			</NavLink>
+			<div className="form-group mt-3">
+				<label htmlFor="makes" className="form-label">
+					Select Make
+				</label>
+				<select
+					name="makes"
+					id="makes"
+					className="form-select"
+					onChange={filterVehicles}
+				>
+					<option value=""></option>
+					{makes.map((m) => (
+						<option key={m.id} value={m.id}>
+							{m.name}
+						</option>
+					))}
+				</select>
+				<button className="btn btn-secondary mt-2" onClick={filterVehicles}>
+					Reset
+				</button>
+			</div>
+
 			<table className="table table-hover table-border my-5">
 				<thead>
 					<tr>
